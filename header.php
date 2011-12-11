be81cd6614f030f903b4a7d95dda6797d94ca15b
Edward Caissie <edward.caissie@gmail.com>
2011-12-11 17:03:31 -0500

    Removed 'author' and 'comment' template files

--- header.php
+++ header.php
@@ -3,16 +3,57 @@
 <?php
 /**
  * Multi
- * a Child-Theme of Desk Mess Mirrored
+ *
+ * A Child-Theme of Desk Mess Mirrored 2.0 - Marble desktop covered with a mix
+ * of old and new items, such as some vintage papers, a stainless steel pen,
+ * and, a hot cup of coffee!
+ *
+ * Desk Mess Mirrored Theme Description: Marble desktop covered with a mix of
+ * old and new items, such as some vintage papers, a stainless steel pen, and, a
+ * hot cup of coffee! Now with more documentation and post-format support for
+ * the following types: asides, quotes and status!
  *
  * @package     Desk_Mess_Mirrored
  * @subpackage  Multi
  *
  * @internal    Child-Theme version should always match the Parent-Theme version
  *
- * Last revised December 2, 2011
+ * @link        http://buynowshop.com/themes/desk-mess-mirrored/multi
+ * @link        https://github.com/Cais/desk-mess-mirrored/multi
+ * @link        http://wordpress.org/extend/themes/multi/ - old version
+ *
+ * @internal    REQUIRES WordPress version 3.1.0 per Desk Mess Mirrored
+ *
+ * @author      Edward Caissie <edward.caissie@gmail.com>
+ * @copyright   Copyright (c) 2009-2011, Edward Caissie
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 2, as published by the
+ * Free Software Foundation.
+ *
+ * You may NOT assume that you can use any other version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to:
+ *
+ *      Free Software Foundation, Inc.
+ *      51 Franklin St, Fifth Floor
+ *      Boston, MA  02110-1301  USA
+ *
+ * The license for this software can also likely be found here:
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @internal    Project To-do List (PTL)
+ * @internal    see readme.txt for pre-2.0 PTL
+ *
+ * Last revised December 11, 2011
  * @version     2.0
  * Modified <title> tag content to use `dmm_wp_title`
+ * Removed the 'author' and 'comment' templates as they are functionally identical to the Desk Mess Mirrored templates
  */
 ?>
 <head profile="http://gmpg.org/xfn/11">
@@ -32,7 +73,7 @@
                     <p><?php bloginfo( 'description' ); ?></p>
                 </div> <!-- #logo -->
                 <div id="cup">
-                    <a href="<?php echo get_feed_link( 'rss2' ); ?>" title="<?php _e('Add this blog to any reader','desk-mess-mirrored'); ?>"><img alt="" src="<?php echo get_stylesheet_directory_uri(); ?>/images/rsscup.png" border="0" /></a>
+                    <a href="<?php echo get_feed_link( 'rss2' ); ?>" title="<?php _e( 'Add this blog to any reader', 'desk-mess-mirrored' ); ?>"><img alt="" src="<?php echo get_stylesheet_directory_uri(); ?>/images/rsscup.png" border="0" /></a>
                 </div><!-- #cup -->
                 <div id="top-navigation-menu">
                     <?php dmm_nav_menu(); ?>
