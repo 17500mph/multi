376b8b4adc603d472f07e3320a87312e7e849fec
Edward Caissie <edward.caissie@gmail.com>
2011-12-17 12:18:58 -0500

    Code formatting

--- header.php
+++ header.php
@@ -59,7 +59,7 @@
  */
 ?>
 <head profile="http://gmpg.org/xfn/11">
-    <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+    <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
     <title><?php dmm_wp_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" media="screen" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
